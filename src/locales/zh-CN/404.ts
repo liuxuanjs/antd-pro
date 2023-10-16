aa7806c79be5fe340d04ae40c721317658367ed3
刘煊 <xuan.liu@shuyun.com>
2023-10-16 15:22:29 +0800

    feat: 404多语言

---
+++ src/locales/zh-CN/404.ts
@@ -0,0 +1,4 @@
+export default {
+  '404.subTitle': '抱歉，您访问的页面不存在。',
+  '404.buttonText': '返回',
+};
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { history } from '@umijs/max';
+import { FormattedMessage, history } from '@umijs/max';
 import { Button, Result } from 'antd';
 import React from 'react';
 
@@ -6,10 +6,10 @@ const NoFoundPage: React.FC = () => (
   <Result
     status="404"
     title="404"
-    subTitle="Sorry, the page you visited does not exist."
+    subTitle={<FormattedMessage id="404.subTitle" />}
     extra={
       <Button type="primary" onClick={() => history.push('/')}>
-        Back Home
+        <FormattedMessage id="404.buttonText" />
       </Button>
     }
   />
